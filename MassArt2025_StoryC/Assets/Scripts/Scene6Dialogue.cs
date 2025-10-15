32fdf6d83451b7139806b4063610fe2ad210b18d
amgonzalez-boop <amgonzalez@massart.edu>
2025-10-15 19:35:27 -0400

    Scene 6 All Dialogue interactions/choices Completed
    Added all dialogue between Hall monitor and player
    Added all interactable buttons
    Needs correct art placements
    Add mini game?

---
+++ MassArt2025_StoryC/Assets/Scripts/Scene6Dialogue.cs
@@ -0,0 +1,246 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class Scene6Dialogue : MonoBehaviour {   
+
+        public int primeInt = 1;        // This integer drives game progress!
+        public TMP_Text Char1name;      //YOU
+        public TMP_Text Char1speech;
+        public TMP_Text Char2name;    //Hall Monitor  
+        public TMP_Text Char2speech;
+      
+        
+        public GameObject DialogueDisplay;
+        
+
+        public GameObject ArtChar2a; 
+        public GameObject ArtChar2b;
+        public GameObject ArtChar2c;
+        public GameObject ArtChar2d;
+        
+
+//public GameObject ArtChar2;
+        public GameObject ArtBG1;
+                public GameObject Choice1a;     
+                public GameObject Choice1b;     
+                public GameObject Choice1c;
+     public GameObject NextScene1Button;
+     public GameObject NextScene2Button;
+     public GameObject nextButton;
+       //public AudioSource audioSource1;
+        private bool allowSpace = true;
+
+// Set initial visibility. Added images or buttons need to also be SetActive(false);
+        void Start(){  
+             DialogueDisplay.SetActive(false);
+             ArtChar2a.SetActive(false);
+             ArtChar2b.SetActive(false);
+             ArtChar2c.SetActive(false);
+             ArtChar2d.SetActive(false);
+             
+             Choice1a.SetActive(false);
+             Choice1b.SetActive(false);
+             Choice1c.SetActive(false);
+
+
+             NextScene1Button.SetActive(false);
+             NextScene2Button.SetActive(false);
+
+             ArtBG1.SetActive(true);
+             nextButton.SetActive(true);
+        }
+
+// Use the spacebar as a faster "Next" button:
+        void Update(){        
+             if (allowSpace == true){
+                 if (Input.GetKeyDown("space")){
+                      Next();
+                 }
+
+                 // secret debug code: go back 1 Story Unit, if NEXT is visible
+                 if (Input.GetKeyDown("p")) {
+                      primeInt -= 2;
+                      Next();
+                 }
+             }
+        }
+
+//Story Units! The main story function.
+//Players hit [NEXT] to progress to the next primeInt:
+public void Next(){
+        primeInt += 1;
+        if (primeInt == 1){
+                // audioSource1.Play();
+        }
+        else if (primeInt == 2){
+                
+                DialogueDisplay.SetActive(true);
+                ArtChar2a.SetActive(true);
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Hall Monitor";
+                Char2speech.text = "I forget why we are here.";
+                
+        }
+       else if (primeInt ==3){
+                
+                Char1name.text = "YOU";
+                Char1speech.text = "Me too.";
+                Char2name.text = "";
+                Char2speech.text = "";
+                //gameHandler.AddPlayerStat(1);
+
+        }
+        else if (primeInt == 4){
+               
+                DialogueDisplay.SetActive(true);
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Hall Monitor";
+                Char2speech.text = "Let's play games.";
+                
+        }
+       else if (primeInt == 5){
+                Char1name.text = "YOU";
+                Char1speech.text = "Ok.";
+                Char2name.text = "";
+                Char2speech.text = "";
+                
+        }
+        else if (primeInt == 6){
+               
+                DialogueDisplay.SetActive(true);
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Hall Monitor";
+                Char2speech.text = "Im bored...";
+                // Turn off the "Next" button, turn on "Choice" buttons
+                nextButton.SetActive(false);
+                allowSpace = false;
+                Choice1a.SetActive(true); // function Choice1aFunct()
+                Choice1b.SetActive(true); // function Choice1bFunct()
+                Choice1c.SetActive(true); // function Choice1bFunct()
+        }
+    //AFTER CHOICE 1
+         //After Choice 1a
+
+        else if (primeInt == 20){
+                Char1name.text = "YOU";
+                Char1speech.text = "WOOOOOO!!";
+                Char2name.text = "";
+                Char2speech.text = "";
+        }
+        else if (primeInt == 21){
+                Char1name.text = "YOU";
+                Char1speech.text = "Damn, what a mess...";
+                Char2name.text = "";
+                Char2speech.text = "";
+        }
+        else if (primeInt == 22){
+                Char1name.text = "YOU";
+                Char1speech.text = "I wanna leave.";
+                Char2name.text = "";
+                Char2speech.text = "";
+                nextButton.SetActive(false);
+                allowSpace = false;
+                NextScene1Button.SetActive(true);
+                NextScene2Button.SetActive(true);
+
+        }
+        //After choice 1b
+
+        else if (primeInt == 30){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Hall Moniter";
+                Char2speech.text = "Me too.";
+        }
+        else if (primeInt == 31){
+             Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Hall Moniter";
+                Char2speech.text = "I wonder if it's chiken nuggies today.";
+                nextButton.SetActive(false);
+                allowSpace = false;
+                NextScene1Button.SetActive(true);
+        }
+        //After choice 1c
+
+        else if (primeInt == 40){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Hall Moniter";
+                Char2speech.text = "Ok.";
+                nextButton.SetActive(false);
+                allowSpace = false;
+                Choice1a.SetActive(true); 
+                Choice1b.SetActive(true);
+        }
+
+
+
+
+    }
+
+
+// FUNCTIONS FOR BUTTONS TO ACCESS (Choice #1 and SceneChanges)
+
+//CHOICE 1:
+        public void Choice1aFunct(){
+                Char1name.text = "YOU";
+                Char1speech.text = "*Jumps on Desk*";
+                Char2name.text = "";
+                Char2speech.text = "";
+                primeInt = 19; // so hitting "NEXT" goes to primeInt==20!
+                Choice1a.SetActive(false);
+                Choice1b.SetActive(false);
+                Choice1c.SetActive(false);
+                nextButton.SetActive(true);
+                allowSpace = true;
+        }
+
+        public void Choice1bFunct(){
+                Char1name.text = "YOU";
+                Char1speech.text = "I'm hungry.";
+                Char2name.text = "";
+                Char2speech.text = "";
+                primeInt = 29; // so hitting "NEXT" goes to primeInt==30!
+                Choice1a.SetActive(false);
+                Choice1b.SetActive(false);
+                Choice1c.SetActive(false);
+                nextButton.SetActive(true);
+                allowSpace = true;
+        }
+
+        public void Choice1cFunct(){
+                Char1name.text = "YOU";
+                Char1speech.text = "I like sharks.";
+                Char2name.text = "";
+                Char2speech.text = "";
+                primeInt = 39; // so hitting "NEXT" goes to primeInt==40!
+                Choice1a.SetActive(false);
+                Choice1b.SetActive(false);
+                Choice1c.SetActive(false);
+                nextButton.SetActive(true);
+                allowSpace = true;
+        }
+
+ //Scene changes
+
+public void SceneChange1(){
+                SceneManager.LoadScene("Scene7");     //cafeteria 
+        }
+
+public void SceneChange2(){
+                SceneManager.LoadScene("Scene8");     //Bathroom
+        }
+
+
+
+
+}
