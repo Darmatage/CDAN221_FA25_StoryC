a6db5190a2345c370c717e214f0ec9b166e77df7
djbianchino-sketch <djbianchino@massart.edu>
2025-10-23 17:00:07 -0400

    Scene 9 finished

--- MassArt2025_StoryC/Assets/Scripts/Scene9Dialogue.cs
+++ MassArt2025_StoryC/Assets/Scripts/Scene9Dialogue.cs
@@ -14,6 +14,8 @@ public class Scene9Dialogue : MonoBehaviour {
         public TMP_Text Char1speech;
         public TMP_Text Char2name;
         public TMP_Text Char2speech;
+        public TMP_Text Char3name;
+        public TMP_Text Char3speech;
         //public TMP_Text Char3name;
         //public TMP_Text Char3speech;
 
@@ -23,6 +25,7 @@ public class Scene9Dialogue : MonoBehaviour {
         public GameObject ArtChar1c;
         public GameObject ArtChar2a;
         public GameObject ArtChar2b;
+        public GameObject ArtSnot;
 
        
         
@@ -48,6 +51,7 @@ public class Scene9Dialogue : MonoBehaviour {
 		ArtChar1c.SetActive(false);
 		ArtChar2a.SetActive(false);
                 ArtChar2b.SetActive(false);
+                ArtSnot.SetActive(false);
 		
 
 		ArtBG1.SetActive(true);
@@ -88,118 +92,288 @@ public class Scene9Dialogue : MonoBehaviour {
                 {
                         DialogueDisplay.SetActive(true);
                         Char1name.text = "YOU";
-                        Char1speech.text = "The Nurses office";
+                        Char1speech.text = "Where am I? This looks like...";
                         Char2name.text = "";
                         Char2speech.text = "";
+                        Char3name.text = "";
+                        Char3speech.text = "";
+                        
                 }
                 else if (primeInt == 2)
+                {
+                        DialogueDisplay.SetActive(true);
+                        Char1name.text = "YOU";
+                        Char1speech.text = "The nurses office...";
+                        Char2name.text = "";
+                        Char2speech.text = "";
+                        Char3name.text = "";
+                        Char3speech.text = "";
+                }
+                else if (primeInt == 3)
+                {       
+                        ArtChar2a.SetActive(true);
+                        DialogueDisplay.SetActive(true);
+                        Char1name.text = "";
+                        Char1speech.text = "";
+                        Char2name.text = "";
+                        Char2speech.text = "";
+                        Char3name.text = "Nurse";
+                        Char3speech.text = "You got beat up bad kid. You trip or something?";
+                }
+                else if (primeInt == 4)
+                {
+                        DialogueDisplay.SetActive(true);
+                        Char1name.text = "YOU";
+                        Char1speech.text = "No. I got beat up.";
+                        Char2name.text = "";
+                        Char2speech.text = "";
+                        Char3name.text = "";
+                        Char3speech.text = "";
+                }
+                else if (primeInt == 5)
+                {       
+                        ArtChar2a.SetActive(true);
+                        DialogueDisplay.SetActive(true);
+                        Char1name.text = "";
+                        Char1speech.text = "";
+                        Char2name.text = "";
+                        Char2speech.text = "";
+                        Char3name.text = "Nurse";
+                        Char3speech.text = "Huh. Well if you feel fine you can head out.";
+                }
+                else if (primeInt == 6)
+                {
+                        ArtChar2a.SetActive(false);
+                        DialogueDisplay.SetActive(true);
+                        Char1name.text = "YOU";
+                        Char1speech.text = "I have to get my milk back.";
+                        Char2name.text = "";
+                        Char2speech.text = "";
+                        Char3name.text = "";
+                        Char3speech.text = "";
+                }
+                else if (primeInt == 7)
                 {
                         ArtChar1a.SetActive(true);
                         Char1name.text = "";
                         Char1speech.text = "";
                         Char2name.text = "Gross ass kid";
                         Char2speech.text = "I'm sick AHHHHHHHHHH!!!!!";
+                        Char3name.text = "";
+                        Char3speech.text = "";
                 }
-                else if (primeInt == 3)
-                {
-                        ArtChar1a.SetActive(true);
+                else if (primeInt == 8)
+                {       
+                        
                         Char1name.text = "YOU";
-                        Char1speech.text = "Oh he is sick ew";
+                        Char1speech.text = "EW WHAT THE FLIP!";
                         Char2name.text = "";
                         Char2speech.text = "";
+                        Char3name.text = "";
+                        Char3speech.text = "";
 
                         // Turn off the "Next" button, turn on "Choice" buttons
+                        //nextButton.SetActive(false);
+                       // allowSpace = false;
+                       //Choice1a.SetActive(true); // function Choice1aFunct()
+                        //Choice1b.SetActive(true); // function Choice1bFunct()
+                }
+                else if (primeInt == 9)
+                {
+                        ArtChar1a.SetActive(false);
+                        ArtChar1b.SetActive(true);
+                        Char1name.text = "";
+                        Char1speech.text = "";
+                        Char2name.text = "Gross ass kid";
+                        Char2speech.text = "Ah";
+                        Char3name.text = "";
+                        Char3speech.text = "";
+                }
+                else if (primeInt == 10)
+                {
+                        ArtChar1a.SetActive(true);
+                        ArtChar1b.SetActive(false);
+                        Char1name.text = "";
+                        Char1speech.text = "";
+                        Char2name.text = "Gross ass kid";
+                        Char2speech.text = "Ah...";
+                        Char3name.text = "";
+                        Char3speech.text = "";
+                }
+                else if (primeInt == 11)
+                {
+                        ArtChar1a.SetActive(false);
+                        ArtChar1b.SetActive(true);
+                        Char1name.text = "";
+                        Char1speech.text = "";
+                        Char2name.text = "Gross ass kid";
+                        Char2speech.text = "Ah...AHHH";
+                        Char3name.text = "";
+                        Char3speech.text = "";
+                }
+                else if (primeInt == 12)
+                {
+                        ArtChar1a.SetActive(true);
+                        ArtChar1b.SetActive(false);
+                        Char1name.text = "";
+                        Char1speech.text = "";
+                        Char2name.text = "Gross ass kid";
+                        Char2speech.text = "Ah...AHHH...";
+                        Char3name.text = "";
+                        Char3speech.text = "";
+                }
+                else if (primeInt == 13)
+                {
+                        ArtChar1a.SetActive(false);
+                        ArtChar1b.SetActive(true);
+                        Char1name.text = "";
+                        Char1speech.text = "";
+                        Char2name.text = "Gross ass kid";
+                        Char2speech.text = "Ah...AHHH...AHHHHH";
+                        Char3name.text = "";
+                        Char3speech.text = "";
+                }
+                else if (primeInt == 14)
+                {
+                        ArtChar1a.SetActive(false);
+                        ArtChar1b.SetActive(true);
+                        Char1name.text = "YOU";
+                        Char1speech.text = "I CANT DODGE IT WHAT DO I DO?";
+                        Char2name.text = "";
+                        Char2speech.text = "";
+                        Char3name.text = "";
+                        Char3speech.text = "";
+                        
                         nextButton.SetActive(false);
                         allowSpace = false;
                         Choice1a.SetActive(true); // function Choice1aFunct()
                         Choice1b.SetActive(true); // function Choice1bFunct()
                 }
+                else if (primeInt == 91)
+                {
+                        ArtChar1a.SetActive(false);
+                        ArtChar1b.SetActive(false);
+                        ArtChar1c.SetActive(true);
+                        Char1name.text = "";
+                        Char1speech.text = "[He blows his nose into the tissue]";
+                        Char2name.text = "";
+                        Char2speech.text = "";
+                        Char3name.text = "";
+                        Char3speech.text = "";
+                }
+                else if (primeInt == 92)
+                {
+                        ArtChar1a.SetActive(true);
+                        ArtChar1b.SetActive(false);
+                        ArtChar1c.SetActive(false);
+                        Char1name.text = "";
+                        Char1speech.text = "";
+                        Char2name.text = "Gross ass kid";
+                        Char2speech.text = "Thanks.";
+                        Char3name.text = "";
+                        Char3speech.text = "";
+                }
+                else if (primeInt == 93)
+                {
+                        ArtChar1a.SetActive(false);
+                        ArtChar2b.SetActive(true);
+                        ArtChar1c.SetActive(false);
+                        Char1name.text = "";
+                        Char1speech.text = "";
+                        Char2name.text = "";
+                        Char2speech.text = "";
+                        Char3name.text = "Nurse";
+                        Char3speech.text = "Here kid! That was nice. \n[She sticks a gold star sticker to your shirt]";
+                        GameHandler.gotGoldStar = true; 
+                }
+                else if (primeInt == 94)
+                {
+                        ArtChar1a.SetActive(false);
+                        ArtChar2b.SetActive(false);
+                        ArtChar1c.SetActive(false);
+                        ArtSnot.SetActive(false);
+                        Char1name.text = "YOU";
+                        Char1speech.text = "I need to get my milk back.";
+                        Char2name.text = "";
+                        Char2speech.text = "";
+                        Char3name.text = "";
+                        Char3speech.text = "";
+                        nextButton.SetActive(false);
+                        allowSpace = false;
+                        NextSceneBathroom.SetActive(true);
+                        NextSceneCafeteria.SetActive(true);
+                }
 
+                 else if (primeInt == 101)
+                {
+                        ArtChar1a.SetActive(false);
+                        ArtChar1b.SetActive(false);
+                        ArtChar1c.SetActive(true);
+                        Char1name.text = "";
+                        Char1speech.text = "[You hold out your hands and your hand gets filled with the sick ass kid's boogers.]";
+                        Char2name.text = "";
+                        Char2speech.text = "";
+                        Char3name.text = "";
+                        Char3speech.text = "";
+                }
+                else if (primeInt == 102)
+                {
+                        ArtChar1a.SetActive(true);
+                        ArtChar1b.SetActive(false);
+                        ArtChar1c.SetActive(false);
+                        Char1name.text = "";
+                        Char1speech.text = "";
+                        Char2name.text = "Gross ass kid";
+                        Char2speech.text = "Ew.";
+                        Char3name.text = "";
+                        Char3speech.text = "";
+                }
+                else if (primeInt == 103)
+                {
+                        ArtChar1a.SetActive(false);
+                        ArtChar2a.SetActive(true);
+                        ArtChar1c.SetActive(false);
+                        Char1name.text = "";
+                        Char1speech.text = "";
+                        Char2name.text = "";
+                        Char2speech.text = "";
+                        Char3name.text = "Nurse";
+                        Char3speech.text = "Ew.";
+                }
+                else if (primeInt == 104)
+                {
+                        ArtChar1a.SetActive(false);
+                        ArtChar2a.SetActive(false);
+                        ArtChar1c.SetActive(false);
+                        ArtSnot.SetActive(true);
+                        Char1name.text = "YOU";
+                        Char1speech.text = "*SNOT OBTAINED*";
+                        Char2name.text = "";
+                        Char2speech.text = "";
+                        Char3name.text = "";
+                        Char3speech.text = "";
+                        primeInt = 93;
 
+                        GameHandler.gotSnot = true; 
+
+                }
 
 
                 //AFTER CHOICE 1a:
-                else if (primeInt == 10)
-                {
-                        Char1name.text = "";
-                        Char1speech.text = "";
-                        Char2name.text = "Gross Ass Kid";
-                        Char2speech.text = "AHHH CHOOO!!!!";
+              
 
-                }
-                else if (primeInt == 11)
-                {
-                        Char1name.text = "";
-                        Char1speech.text = "";
-                        Char2name.text = "Gross Ass Kid";
-                        Char2speech.text = "Thank u";
-                }
-                else if (primeInt == 12)
-                {
-                        Char1name.text = "Nurse";
-                        Char1speech.text = "You get a gold star!";
-                        Char2name.text = "";
-                        Char2speech.text = "";
-                }
-
-                else if (primeInt == 13)
-                {
-                        Char1name.text = "YOU";
-                        Char1speech.text = "Ok I am leave now.";
-                        Char2name.text = "";
-                        Char2speech.text = "";
-                        // Turn off the "Next" button, turn on "Choice" buttons
-                        nextButton.SetActive(false);
-                        allowSpace = false;
-                       // function Choice2aFunct()
-
-                }
+           
 
         
 
 
                 // after choice 1b
-                else if (primeInt == 20)
-                {
-                        Char1name.text = "";
-                        Char1speech.text = "";
-                        Char2name.text = "Gross Ass Kid";
-                        Char2speech.text = "AHHH CHOOO!!!!";
-                }
-
-                else if (primeInt == 21)
-                {
-                        Char1name.text = "Gross Ass Kid";
-                        Char1speech.text = "[SNEEZES into your hands]";
-                        Char2name.text = "";
-                        Char2speech.text = "";
-                }
-
-                else if (primeInt == 22)
-                {
-                        Char1name.text = "";
-                        Char1speech.text = "";
-                        Char2name.text = "Gross ass kid";
-                        Char2speech.text = "ew";
-                }
-
-                else if (primeInt == 23)
-                {
-                        Char1name.text = "Nurse";
-                        Char1speech.text = "ew";
-                        Char2name.text = "";
-                        Char2speech.text = "";
-                        // Turn off the "Next" button, turn on "Scene" button/s
-                        nextButton.SetActive(false);
-                        allowSpace = false;
-                       
-                        
-                }
+               
 
 
                 //AFTER choice 2a: after "I hate your nephew"
               
-
+//[You hold out your hands and your hand gets filled with the sick ass kid's boogers.]
 
                 
 
@@ -211,11 +385,16 @@ public class Scene9Dialogue : MonoBehaviour {
 
 //CHOICE 1: 
         public void Choice1aFunct(){
-                Char1name.text = "YOU";
-                Char1speech.text = "Give him tissues";
-                Char2name.text = "";
-                Char2speech.text = "";
-                primeInt = 9; 
+                ArtChar1a.SetActive(false);
+                ArtChar1b.SetActive(false);
+                ArtChar1c.SetActive(true);
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Gross ass kid";
+                Char2speech.text = "AHHHHHCHOOOOOOOOOOOOOO";
+                Char3name.text = "";
+                Char3speech.text = "";
+                primeInt = 90; 
                 Choice1a.SetActive(false);
                 Choice1b.SetActive(false);
                 nextButton.SetActive(true);
@@ -225,12 +404,18 @@ public class Scene9Dialogue : MonoBehaviour {
 
 //CHOICE 2:
          public void Choice2aFunct(){
-                Char1name.text = "YOU";
-                Char1speech.text = "Hold out your hands";
-                Char2name.text = "";
-                Char2speech.text = "";
-                primeInt = 19; 
-                
+                ArtChar1a.SetActive(false);
+                ArtChar1b.SetActive(false);
+                ArtChar1c.SetActive(true);
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Gross ass kid";
+                Char2speech.text = "AHHHHHCHOOOOOOOOOOOOOO";
+                Char3name.text = "";
+                Char3speech.text = "";
+                primeInt = 100; 
+                Choice1a.SetActive(false);
+                Choice1b.SetActive(false);
                 nextButton.SetActive(true);
                 allowSpace = true;
         }
